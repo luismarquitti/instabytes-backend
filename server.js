154bbb5da3339574e287bcc772d4f71ef80a4074
Luis Eduardo Angelini Marquitti <luismarquitti@gmail.com>
2024-11-25 11:41:01 -0300

    Finalizando Aula 03

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 import express from "express";
+import routes from "./src/routes/postsRoutes.js";
 
 const app = express();
+
+routes(app);
+
 app.listen(3000, () => {
     console.log("Servidor escutando...");
-});
-
-app.get("/api", (req, res) => {
-    res.status(200).send("A Torre Eiffel iluminada à noite, com milhares de luzes cintilando, criando um espetáculo mágico em Paris.");
 });
